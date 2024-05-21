40c5d5ccf0aec712343e12cbc4c0a2e72e253032
Christianmagni97 <christianmagni28@gmail.com>
2024-05-21 12:40:44 +0200

    push snake 14

---
+++ snacks14/script14.js
@@ -0,0 +1,7 @@
+const secondTime = Number. parseInt(prompt('quanti secondi vuoi attendere?');
+console.log(secondTime);
+const clock = setInterval(tempoSceltoUtente, secondTime);
+function tempoSceltoUtente(){
+    alert('fine timer');
+
+}
