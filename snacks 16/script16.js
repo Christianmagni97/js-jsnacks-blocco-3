b91f1381aa687f21665a66974cccd86f37aabc96
Christianmagni97 <christianmagni28@gmail.com>
2024-05-23 12:19:29 +0200

    inserito auto

---
+++ snacks 16/script16.js
@@ -0,0 +1,13 @@
+const auto = [
+    {name: 'audi',model: 'a1', supply: 'gas'}
+    {name:'mercedes',model:'classeA' supply:'gas'}
+    {name:'bmw',model:'m4' supply:'gas'}
+    {name:'fiat',model:'500' supply:'hibrid'}
+    {name:'ferrari',model:'california' supply:'gas'}
+    {name:'bentley',model:'stonks' supply:'hibrid'}
+    {name:'rangeRover',model:'evoque' supply:'hibrid'}
+    {name:'toyota',model:'yaris' supply:'hibrid'}
+    {name:'suzuki',model:'fg6' supply:'gas'}
+    {name:'dacia',model:'classeTY' supply:'eletric'}
+    
+]
