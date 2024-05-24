d0f768049e5d10a0e76efc4c500426d50b3ec2c0
Christianmagni97 <christianmagni28@gmail.com>
2024-05-24 11:28:29 +0200

    aggiunti varietà di zucchine

---
+++ snacksZucchine/scriptzucchine.js
@@ -0,0 +1,33 @@
+const zucchine = [
+    {
+        name: 'zucchinaChiara', lughezza: '8', peso: '2'
+    },
+    {
+        name: 'zucchinaChiara', lughezza: '9', peso: '2'
+    },
+    {
+        name: 'zucchina', lughezza: '4', peso: '1'
+    },
+    {
+        name: 'zucchina', lughezza: '6', peso: '1'
+    },
+    {
+        name: 'zucchinaChiara', lughezza: '14', peso: '3'
+    },
+    {
+        name: 'zucchinaChiara', lughezza: '16', peso: '4'
+    },
+    {
+        name: 'zucchina', lughezza: '3', peso: '1'
+    },
+    {
+        name: 'zucchinaChiara', lughezza: '13', peso: '3'
+    },
+    {
+        name: 'zucchina', lughezza: '8', peso: '2'
+    },
+    {
+        name: 'zucchina', lughezza: '20', peso: '4'
+    },
+]
+console.log(zucchine);
